5b6986ee3b09ca02eb554c26e53592b9a375757e
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-01-18 20:37:29 -0300

    add rotar cadastrarSequencia & cadastrados

--- public/js/myJS.js
+++ public/js/myJS.js
@@ -1,1 +1,25 @@
-console.log("funcionando o arquivo js!")
+function callForm(number){
+    let $registerForm = document.getElementById("registerForm")
+    let inputNumer = ""
+
+    for(let cont = 1; cont <= number; cont++){
+       inputNumer += `<input  id="sequence${cont}" type="number"> ` 
+    }
+
+    $registerForm.innerHTML = ` <input id="typeSequence" type="hidden" value="${number}">
+
+                                <label for="sequence">sequencia</label>
+                                ${inputNumer}
+                                <br>
+
+                                <label for="concurso">Concurso</label>
+                                <input id="concurso" type="number">
+                                <br> 
+
+                                <labl  for="dataDoConcurso">Data do Concurso</labl>
+                                <input id="dataDoConcurso" type="date">
+                                <br>
+
+                                <button type="submit">Enviar Formulário</button>
+                            `
+}
--- routs/admin.js
+++ routs/admin.js
@@ -5,8 +5,24 @@ router.get("/",(req,res)=>{
     res.render("admin/index")
 });
 
+router.get("/cadastrarSequencia", (req,res)=>{
+    res.render("admin/cadastrarSequencia")
+})
+
+
+
+//---\-------------------------------------------------------------------em produção
+router.get("/cadastrados/:parametro", (req,res)=>{
+    let obj = {
+        sequencia: req.params.parametro
+    }
+
+    res.render("admin/sequenciaCadastrada",{obj})
+})
+
 router.get("/testeflash",(req, res)=>{
     req.flash("success_msg", "O connect flash ta funcionandoooooo!!!!!!")
     res.redirect("/")
 })
-module.exports = router
+
+module.exports = router;
